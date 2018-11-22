b5e42725723e807e91297bc67797763ffeaf6231
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-22 13:43:44 -0800

    moved to notes script

--- scripts/01_intro_shiny_basics.R
+++ scripts/01_intro_shiny_basics.R
@@ -6,21 +6,7 @@
 # title: Datacamp
 #        Building Web Applications in R with Shiny
 #        Introduction and Shiny Basics
-#
 
-
-############### Anatomy of a Shiny App ##################
-#
-# load data before ui definition
-#
-# lay out the user interface; controls layout, appearance
-# ui <- fluidPage()
-
-# server function; contains instructions needed to build app
-# server <- function(input, output){}
-
-# calling the shiny app function that combines the above components
-# shinyApp(ui = ui, server = server)
 
 library("tidyverse")
 library("shiny")
