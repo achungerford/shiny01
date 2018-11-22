877226b8eeca53488cc870263aaac01fcc40f3ce
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-22 13:21:15 -0800

    created notes script

---
+++ notes.R
@@ -0,0 +1,26 @@
+# 
+# author: Alexander C. Hungerford
+#
+# created: 21 November 2018 
+# 
+# title: Notes for course:
+#
+#        Datacamp
+#        Building Web Applications in R with Shiny
+#        Introduction and Shiny Basics
+
+
+############### Anatomy of a Shiny App ##################
+#
+# load data before ui definition
+#
+# lay out the user interface; controls layout, appearance
+# ui <- fluidPage()
+
+# server function; contains instructions needed to build app
+# server <- function(input, output){}
+
+# calling the shiny app function that combines the above components
+# shinyApp(ui = ui, server = server)
+
+
